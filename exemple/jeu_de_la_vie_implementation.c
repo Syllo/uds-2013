669e3cdf113dcaad5bdacbcd3ddb9ee80cc58b02
Maxime SCHMITT <maxime.schmitt91@gmail.com>
2013-06-13 17:01:53 +0200

    Fini l'exemple sur le jeu de la vie et ajout de makefile

--- exemple/jeu_de_la_vie_implementation.c
+++ exemple/jeu_de_la_vie_implementation.c
@@ -3,10 +3,10 @@
 #include <jeu_de_la_vie_implementation.h>
 
 #define N 10
-typedef enum {Mort,Vie} Vivant;
+#define SEED 12
 
-int t[N][N];
-int temp[N-2][N-2];
+Vivant t[N][N];
+Vivant temp[N-2][N-2];
 
 Vivant game_of_life (Vivant mm,Vivant bm, Vivant bg, Vivant bd, Vivant mg, Vivant md, Vivant hg, Vivant hm, Vivant hd){
     int nombreEnVie;
@@ -23,21 +23,19 @@ Vivant game_of_life (Vivant mm,Vivant bm, Vivant bg, Vivant bd, Vivant mg, Vivan
 }
 
 void compute(int i,int j){
-    temp[i-1][j-1]=game_of_life(t[i],[j],t[i-1][j],t[i-1][j-1],t[i-1][j+1],[i][j-1],[i][j+1],[i+1][j-1],[i+1][j],[i+1][j+1]);
+    temp[i-1][j-1]=game_of_life(t[i][j],t[i-1][j],t[i-1][j-1],t[i-1][j+1],t[i][j-1],t[i][j+1],t[i+1][j-1],t[i+1][j],t[i+1][j+1]);
 }
 
-void c opie(int i, int j){
+void copie(int i, int j){
     t[i][j]=temp[i-1][j-1];
 }
 
 int max(int i, int j){
-
-   i>j ? return i : return j;
+   return i>j ? i : j;
 }
 
-void 
-ame_of_life_multifor{
-     for(int i=1; i<N-1; i++){
+void game_of_life_multifor(){
+    for(int i=1; i<N-1; i++){
         for(int j=1; j<N-1; j++){
             compute(i,j);
             if(i>=4){
@@ -45,9 +43,61 @@ ame_of_life_multifor{
             }
         }
     }
-      for(i=max(4,N-1); i<=N+1; i++){
-        for(j=1; j<N-1; j++){
+    for(int i=max(4,N-1); i<=N+1; i++){
+        for(int j=1; j<N-1; j++){
             copie(i-3,j);
         }
     }
+    return;
+}
+
+void game_of_life_normal(){
+    for(int i=1; i<N-1; i++){
+        for(int j=1; j<N-1; j++){
+            compute(i,j);
+        }
+    }
+    for(int i=1; i<N-1; i++){
+        for(int j=1; j<N-1; j++){
+            copie(i,j);
+        }
+    }
+    return;
+}
+
+void initialise_alea(){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            t[i][j]=(Vivant) rand()%2;
+        }
+    }
+}
+
+int main (){
+    FILE * jdv_normal_data=fopen("jdv_normal.data","w");
+    FILE * jdv_multifor_data=fopen("jdv_multifor.data","w");
+    if(jdv_multifor_data==NULL || jdv_normal_data==NULL){
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+    srand(SEED);
+    initialise_alea();
+    game_of_life_normal();
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            fprintf(jdv_normal_data, "%d %d %d\n", i, j, t[i][j]);
+        }
+    }
+    srand(SEED); //réinitialise le rand pour obtenir les mêmes valeurs
+    initialise_alea();
+    game_of_life_multifor();
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            fprintf(jdv_multifor_data, "%d %d %d\n", i, j, t[i][j]);
+        }
+    }
+    if(fclose(jdv_multifor_data)==EOF || fclose(jdv_normal_data)==EOF){
+        perror("fclose");
+    }
+    return EXIT_SUCCESS;
 }
--- exemple/jeu_de_la_vie_implementation.h
+++ exemple/jeu_de_la_vie_implementation.h
@@ -1,6 +1,73 @@
 #ifndef __JEU_DE_VIE_IMPLEMENTATION_H
 #define __JEU_DE_VIE_IMPLEMENTATION_H
 
+typedef enum {Mort,Vie} Vivant;
 
+/**
+ * \brief Calcule l'état suivant de la cellule par rapport aux cellules environnantes
+ * \param mm Cellule du centre (celle qui évolue)
+ * \param bm Cellule bas milieu
+ * \param bg Cellule bas gauche
+ * \param bd Cellule bas droite
+ * \param mg Cellule milieu gauche
+ * \param md Cellule milieu droite
+ * \param hg Cellule haut gauche
+ * \param hm Cellule haut milieu
+ * \param hd Cellule haut droite
+ * \return L'état de la cellule dans 1 pas de temps
+ */
+
+Vivant game_of_life (Vivant mm,Vivant bm, Vivant bg, Vivant bd, Vivant mg, Vivant md, Vivant hg, Vivant hm, Vivant hd);
+
+/**
+ * \brief Remplit la grille temp avec les valeurs du temps+1
+ * \param i la ligne a calculer
+ * \param j la colonne a calculer
+ * \pre 1<=i<N
+ * \pre 1<=j<N
+ */
+
+void compute(int i,int j);
+
+/*
+ * \brief Copie de la grille temporaire vers la grille principale
+ * \param i La ligne de la grille principal a remplir
+ * \paral j La colonne de la grille principal a remplir
+ */
+
+void copie(int i, int j);
+
+/**
+ * \brief Renvoie le max entre i et j
+ * \param i un entier signé
+ * \param j un entier signé
+ * \return Le plus grand de i et j
+ */
+
+int max(int i, int j);
+
+/**
+ * Version multifor du jeu de la vie(voit jeu_de_la_vie.c
+ */
+
+void game_of_life_multifor();
+
+/**
+ * Version itérative normal du jeu de la vie (voir jeu_de_la_vie.c)
+ */
+
+void game_of_life_normal();
+
+/**
+ * \brief Initialise la grille avec des valeurs aléatoires
+ */
+
+void initialise_alea();
+
+/**
+ * Lance le jeu de la vie avec et sans multifor et sauvegarde les résultats dans le dossier courant sous la forme "ligne colonne valeur"
+ */
+
+int main();
 
 #endif // __JEU_DE_VIE_IMPLEMENTATION_H
